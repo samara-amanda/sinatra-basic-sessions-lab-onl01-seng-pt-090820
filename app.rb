03d980929dc76b1229e4fe06e5a65d227309fca0
Samara Grant <samara.amanda01@gmail.com>
2020-11-27 08:46:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do
-
+    @session = session
     erb :index
   end
 
   post '/checkout' do
-    @session = session
+
   end
 end
